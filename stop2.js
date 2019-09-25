5b3564153cc9c74d1d99045956195c8a6b1d5c39
cpionhost <55793060+cpionhost@users.noreply.github.com>
2019-09-25 13:59:25 +0200

    Add files via upload

---
+++ stop2.js
@@ -0,0 +1,8 @@
+window.addEventListener("play", function(evt)
+{
+    if(window.$_currentlyPlaying && window.$_currentlyPlaying != evt.target)
+    {
+        window.$_currentlyPlaying.pause();
+    } 
+    window.$_currentlyPlaying = evt.target;
+}, true);
